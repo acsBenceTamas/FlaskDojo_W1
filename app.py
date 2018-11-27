27c1320f4262396993d6bb0ab7469f71b37ba667
Ács Bence Tamás <acs.bence.tamas@gmail.com>
2018-11-27 10:42:24 +0100

    save and load does not work yet

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request
+import csv
 
 app = Flask(__name__)
-
-counts = {'GET': 0, 'POST': 0, 'PUT': 0, 'DELETE': 0}
 
 
 @app.route('/request-counter/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def request_counter():
-    global counts
+    counts = load_counts()
     counts[request.method] += 1
+    save_counts(counts)
     return redirect(url_for('index'))
 
 
 @app.route('/statistics/')
 def statistics():
-    global counts
+    counts = load_counts()
     return render_template('statistics.html', counts=counts)
 
 
@@ -22,6 +22,26 @@ def statistics():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+def load_counts():
+    counts = {}
+    with open("counts.txt") as f:
+        csv_reader = csv.reader(f, delimiter=':')
+        for row in csv_reader:
+            print(row)
+            counts[row[0]] = int(counts[row[1]])
+    return counts
+
+
+def save_counts(counts):
+    print("counts")
+    print(counts)
+    with open("counts.txt") as f:
+        csv_writer = csv.writer(f, delimiter=':')
+        for row in counts:
+            csv_writer.writerow([row, counts[row]])
+    return 1
 
 
 if __name__ == '__main__':
